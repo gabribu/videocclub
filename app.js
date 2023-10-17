be0adb0ad5eb5d5d61f5f28de69968ca1cb9f03e
Gabriel Bustillos <a353267@uach.mx>
2023-10-17 13:31:42 -0600

    Se incia con el ODM

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const directorsRouter = require('./routes/directors');
 
 const app = express();
+//mongodb://<dbUser>?:<dbPass>?@<url>:<port>/<dbName>
+const url = "mongodb://localhost:27017/dbVideoClub"
+mongoose.connect(url);
+
+const db = mongoose.connection;
+db.on('open', ()=> {
+  console.log("Connection OK");
+});
+
+db.on('error', ()=> {
+  console.log("Connection Failed")
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
---
+++ models/director.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+// Schema: Estructura que representa la coneccione que se encuentra en la base de datos
+const schema = mongoose.Schema({
+    _name:String,
+    _lastName:String
+});
+// Clase: Para los objetos
+class Director {
+    constructor(name, lastName){
+        this._name = name;
+        this._lastName = lastName;
+    }
+
+    get name(){
+        return this._name;
+    }
+    set name(v){
+        this._name = v;
+    }get lastName(){
+        return this._lastNameame;
+    }
+    set lastName(v){
+        this._lastName = v;
+    }
+}
+
+schema.loadClass(Director);
+module.exports = mongoose.model('Director', schema);
